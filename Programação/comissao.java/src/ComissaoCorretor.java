6f67a300edb289460c3d773cd6b13e7b323f5f54
IsabelaLuma <isabelaluma@gmail.com>
2025-05-09 10:34:23 -0300

    Update ComissaoCorretor.java

--- Programação/comissao.java/src/ComissaoCorretor.java
+++ Programação/comissao.java/src/ComissaoCorretor.java
@@ -1,3 +1,9 @@
+/*
+A empresa paga ao corretor uma comissão calculada de acordo com o valor de suas vendas.
+ Se o valor da venda de um corretor for maior que R$ 50.000,00 a comissão será de 12% do valor vendido.
+ Se o valor da venda do corretor estiver entre R$ 30.000,00 e R$ 50.000,00 (incluindo extremos) a comissão será de 9,5%.
+ Em qualquer outro caso, a comissão será de 7%. Escreva um programa onde será informado nome do corretor e o valor da venda, após isto o programa irá calcular o valor da comissão.
+*/
 import java.util.Scanner;
 public class ComissaoCorretor {
     public static void main(String[] args) {
