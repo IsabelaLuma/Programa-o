29080a9db9be7b97722ba16ebb9354de92bcdfec
IsabelaLuma <isabelaluma@gmail.com>
2025-05-09 10:29:52 -0300

    Update QuitandaFrutas.java

--- Programação/quitanda.java/src/QuitandaFrutas.java
+++ Programação/quitanda.java/src/QuitandaFrutas.java
@@ -1,3 +1,7 @@
+/*Uma quitanda está vendendo frutas com a seguinte tabela de preços. 
+Se o cliente comprar mais de 8 Kg em frutas ou o valor total da compra ultrapassar R$ 25,00, receberá ainda um desconto de 10% sobre este total.
+ Escreva um algoritmo para ler a quantidade (em Kg) de morangos
+e a quantidade (em Kg) de maças adquiridas e escreva o valor a ser pago pelo cliente.*/
 import java.util.Scanner;
 public class QuitandaFrutas {
     public static void main(String[] args) {
