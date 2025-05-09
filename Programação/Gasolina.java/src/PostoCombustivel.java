92797e8c450c27764add60d7f8015fe94559a403
IsabelaLuma <isabelaluma@gmail.com>
2025-05-09 10:31:54 -0300

    Update PostoCombustivel.java

--- Programação/Gasolina.java/src/PostoCombustivel.java
+++ Programação/Gasolina.java/src/PostoCombustivel.java
@@ -1,3 +1,8 @@
+/*
+Um posto está vendendo combustíveis com a seguinte tabela de descontos. 
+Escreva um programa que leia o número de litros vendidos e o tipo de combustível (codificado da seguinte forma: A-álcool, G-gasolina), 
+calcule e imprima o valor a ser pago pelo cliente sabendo-se que o preço do litro da gasolina é R$ 3,30 e o preço do litro do álcool é R$ 2,90.
+*/
 import java.util.Scanner;
 public class PostoCombustivel {
     public static void main(String[] args) {
